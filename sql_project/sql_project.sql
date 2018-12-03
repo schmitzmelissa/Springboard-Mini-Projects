df46162d195eaa88b895d5992b77e1f885895a3f
Melissa Schmitz <schmitma@lemoyne.edu>
2018-12-02 22:09:47 -0500

    Question 1/10 query complete.

--- sql_project/sql_project.sql
+++ sql_project/sql_project.sql
@@ -22,8 +22,15 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Please list the names of the facilities that do. */
 
+SELECT *
+FROM 'Facilities'
+WHERE membercost > 0
+
+/* RESULTS:
+Tennis Court 1, Tennis Court 2, Massage Room 1, Massage Room 2, Squash Court */
 
 /* Q2: How many facilities do not charge a fee to members? */
+
 
 
 /* Q3: How can you produce a list of facilities that charge a fee to members,
