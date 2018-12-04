b6b46d9af3357e8dbd7f315e2f3157a68144b7a5
Melissa Schmitz <schmitma@lemoyne.edu>
2018-12-03 22:22:18 -0500

    Completed question 2/10.

--- sql_project/sql_project.sql
+++ sql_project/sql_project.sql
@@ -31,17 +31,32 @@ Tennis Court 1, Tennis Court 2, Massage Room 1, Massage Room 2, Squash Court */
 
 /* Q2: How many facilities do not charge a fee to members? */
 
+SELECT *
+FROM 'Facilities'
+WHERE membercost = 0
 
+/* RESULTS:
+4 */
 
 /* Q3: How can you produce a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost?
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 
+SELECT facid,
+       name,
+       membercost,
+       monthlymaintenance
+FROM 'Facilities'
+WHERE membercost < (monthlymaintenance * .2)
+
+/* RESULTS:
+QUERY FAILED TO RUN */
 
 /* Q4: How can you retrieve the details of facilities with ID 1 and 5?
 Write the query without using the OR operator. */
 
+/* ??? */
 
 /* Q5: How can you produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
